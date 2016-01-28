f3378ac383288dff51a585355ec3d120ed1d71f1
pierre drecourt <pierre.drecourt@gmail.com>
2016-01-28 17:39:20 +0100

    1st Stable Version + 1st comments

--- app/src/main/java/com/pedroprojets/trainingprojet1/MainActivity.java
+++ app/src/main/java/com/pedroprojets/trainingprojet1/MainActivity.java
@@ -1,12 +1,18 @@
 package com.pedroprojets.trainingprojet1;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
+    // Première méthode appelée. Aussi appelée aprés qu'une App ait été tuée par le système
+    // Bundle au 1er lancement de l'app / aprés qu'elle ait été quittée normalement = NULL
+    // Bundle au retour de l'app aprés perte de focus ou redémarrage = Possibilité de données sauvegardées
     protected void onCreate(Bundle savedInstanceState) {
+        // L'instruction 'super' indique qu'on fait appel à une méthode ou à un attribut de la superclasse
+        // de la méthode, aka Classe parente (Activity pour notre exemple)
+        // On appelle donc ici la méthode onCreate d'Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
